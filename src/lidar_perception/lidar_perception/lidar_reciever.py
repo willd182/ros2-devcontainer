9e935fa100a6f3425e32b36cc74a93d0741cf395
willd182 <willd182@erau.edu>
2026-06-15 18:29:07 +0000

    Define QoS for lidar_reciever

--- src/lidar_perception/lidar_perception/lidar_reciever.py
+++ src/lidar_perception/lidar_perception/lidar_reciever.py
@@ -4,15 +4,23 @@ from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2
 import sensor_msgs_py.point_cloud2 as pc2
 
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
+
 class PointCloudListener(Node):
 	def __init__(self):
 		super().__init__("lidar_receiver")
+
+		qos = QoSProfile(
+			reliability=QoSReliabilityPolicy.RELIABLE,
+			history=QoSHistoryPolicy.KEEP_LAST,
+			depth=1
+		)
 
 		self.subscription = self.create_subscription(
 			PointCloud2,
 			'/points',
 			self.pointcloud_callback,
-			10
+			qos
 			)
 		
 		self.get_logger().info("Starting lidar reciever node!")
@@ -21,10 +29,12 @@ class PointCloudListener(Node):
 		points = pc2.read_points(msg, field_names=("x","y","z"), skip_nans=True)
 		
 		count = 0
+
 		for p in points:
 			count += 1
-		
-		self.get_logger().info(f"Received point cloud with {count} points")
+
+		ts = msg.header.stamp.sec
+		self.get_logger().info(f"Received point cloud with {count} points at t={ts}")
 
 def main(args=None):
 	rclpy.init(args=args)
